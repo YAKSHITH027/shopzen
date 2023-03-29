49d7286df60f278e490cc7f5bfdb5f01b0988974
yakshith <yakshith027@gmail.com>
2023-03-29 20:56:30 +0530

    landing and profile basic done

---
+++ frontend/src/components/home/AutoSlider.jsx
@@ -0,0 +1,61 @@
+import { Image } from '@chakra-ui/react'
+import React, { Component } from 'react'
+import Slider from 'react-slick'
+
+export default class AutoSlider extends Component {
+  render() {
+    var settings = {
+      infinite: true,
+
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      initialSlide: 0,
+      autoplay: true,
+      speed: 7000,
+      arrows: false,
+      autoplaySpeed: 1000,
+      cssEase: 'linear',
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+      ],
+    }
+    return (
+      <div>
+        <Slider {...settings}>
+          {this.props.data.map((item) => {
+            return (
+              <Image
+                width='150px'
+                height={'353px'}
+                src={item.image}
+                key={item.id}
+              />
+            )
+          })}
+        </Slider>
+      </div>
+    )
+  }
+}
---
+++ frontend/src/utils/HomeSliderData/newArrival.js
@@ -0,0 +1,58 @@
+export const newArrivalData = [
+  {
+    image:
+      'https://images.dailyobjects.com/marche/assets/images/other/halo-magsafe-case-with-stand-new-arrival-homepage-image.png?tr=cm-pad_resize,v-2,dpr-1',
+    id: 5,
+    color: 'available in multiple models',
+    title: 'Halo Phone Cases',
+  },
+  {
+    image:
+      'https://images.dailyobjects.com/marche/assets/images/other/convoy-leather-adapter-case-new-arrival-homepage-image.png?tr=cm-pad_resize,v-2,dpr-1',
+    id: 6,
+    color: 'available in 5 colors',
+    title: 'Macbook Adapter Case',
+  },
+  {
+    image:
+      'https://images.dailyobjects.com/marche/assets/images/other/condo-watch-charging-stand-new-arrival-imge.png?tr=cm-pad_resize,v-2,dpr-1',
+    id: 7,
+    color: 'available in black color',
+    title: 'Apple Watch Charger',
+  },
+  {
+    image:
+      'https://images.dailyobjects.com/marche/assets/images/other/group-6681.jpg?tr=cm-pad_crop,v-2,dpr-1',
+    id: 8,
+    color: 'available in 6 colors',
+    title: 'Pedal Backpack',
+  },
+  {
+    image:
+      'https://images.dailyobjects.com/marche/assets/images/other/group-6679.jpg?tr=cm-pad_crop,v-2,dpr-1',
+    id: 1,
+    color: 'available in 3 colors',
+    title: 'Urban briefcase',
+  },
+  {
+    image:
+      'https://images.dailyobjects.com/marche/assets/images/other/group-6680.jpg?tr=cm-pad_crop,v-2,dpr-1',
+    id: 2,
+    color: 'available in 4 colors',
+    title: 'Align Notebooks',
+  },
+  {
+    image:
+      'https://images.dailyobjects.com/marche/assets/images/other/group-6678.jpg?tr=cm-pad_crop,v-2,dpr-1',
+    id: 3,
+    color: 'available in 3 colors',
+    title: 'Surge 4-in-1 cable',
+  },
+  {
+    image:
+      'https://images.dailyobjects.com/marche/assets/images/other/group-6677.jpg?tr=cm-pad_crop,v-2,dpr-1',
+    id: 4,
+    color: 'available in 3 colors',
+    title: 'Laptop cover',
+  },
+]
