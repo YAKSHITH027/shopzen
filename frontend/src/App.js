3293c69ce0ae5b1bd09b5efa6e99cd3cab11e427
yakshith <yakshith027@gmail.com>
2023-03-30 12:41:13 +0530

    some small changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,13 @@ import Home from './pages/home/Home'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import Profile from './pages/profile/Profile'
-
+import Allrouter from './pages/Allrouter'
 function App() {
   return (
     <div className='App'>
       {/* <Home /> */}
       {/* <Profile /> */}
+      <Allrouter />
     </div>
   )
 }
