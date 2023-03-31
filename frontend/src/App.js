4c832868289769e808adfc07f2f58acd74c6b7bc
Codex-Swapnil1 <swapnilparchakewrd2@gmail.com>
2023-03-31 12:10:03 +0530

    updated the branch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,29 +6,20 @@ import Profile from './pages/profile/Profile'
 import Cart from "./pages/cart/Cart"
 import Checkout from "./pages/Checkout/Checkout"
 import Allrouter from './pages/Allrouter'
-<<<<<<< HEAD
-import Product from './pages/Product/Product'
-=======
 
 import Topbar from './Adminside/scene/global/Topbar'
 import { Box, Flex } from '@chakra-ui/layout'
 import Sidebar from './Adminside/scene/global/Sidebar'
 import Dashboard from './Adminside/scene/Dashboard'
 
->>>>>>> 639f5831a4d810beca640ff3d04f065079f326bf
 function App() {
   return (
     <div className='App'>
       {/* <Home /> */}
       {/* <Profile /> */}
-<<<<<<< HEAD
-      <Product/>
-      {/* <Allrouter /> */}
-=======
       {/*<Cart/>*/}
       {/*<Checkout/>*/}
       <Allrouter />
->>>>>>> 639f5831a4d810beca640ff3d04f065079f326bf
     </div>
   )
 }
