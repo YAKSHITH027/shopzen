872be0f8cf311a04adb735e25d7de69449c47c09
yakshith <yakshith027@gmail.com>
2023-04-01 14:56:36 +0530

    seems right

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,10 +1,18 @@
 // all crud with some query
-const express = require("express")
-const { ProductGet, ProductPost, SingleProductGet } = require("../controller/product.controller")
+const express = require('express')
+const {
+  ProductGet,
+  ProductPost,
+  SingleProductGet,
+  updateProduct,
+  deleteProduct,
+} = require('../controller/product.controller')
 
-const product=express.Router()
+const product = express.Router()
 
-product.get("/",ProductGet)
-product.get("/:id",SingleProductGet)
-product.post("/add",ProductPost)
-module.exports={product}
+product.get('/', ProductGet)
+product.get('/:id', SingleProductGet)
+product.post('/add', ProductPost)
+product.patch('/update/:productId', updateProduct)
+product.delete('/delete/:productId', deleteProduct)
+module.exports = { product }
