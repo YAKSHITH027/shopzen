f79bb332143c1b35c9c37d90ef972698d7c4002f
yakshith <yakshith027@gmail.com>
2023-04-02 12:52:38 +0530

    checkout done

--- frontend/src/components/products/ProductCart.jsx
+++ frontend/src/components/products/ProductCart.jsx
@@ -1,21 +1,41 @@
-import { Box, Image, Text } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
-function ProductCart({image,title,price,ogprice,is_new, id}){
-    return (
-        <Link to={`/product/${id}`}>
-        <Box fontFamily="sans-serif" textAlign="start">
-            <Box>
-                <Image src={image} alt="Image" backgroundColor="#F7F7F7" />
-            </Box>
-            <Box>
-                <Text fontSize={["13px","14px","15px","16px","19px","19px"]}>{title}</Text>
-            </Box>
-            <Box display="flex" gap="7px">
-                <Text fontWeight="bold" fontSize={["16px","17px","19px","18px","21px","21px"]}>Rs.{price}</Text>
-                <Text fontWeight="bold" fontSize={["16px","17px","19px","18px","21px","21px"]} color="gray" textDecoration="line-through">{ogprice}</Text>
-            </Box>
+import { Box, Image, Text } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
+function ProductCart({ image, title, price, ogprice, is_new, id }) {
+  return (
+    <Link to={`/product/${id}`}>
+      <Box fontFamily='sans-serif' textAlign='start'>
+        <Box>
+          <Image src={image} alt='Image' backgroundColor='#F7F7F7' />
         </Box>
-        </Link>
-    )
+        <Box>
+          <Text
+            fontSize={['13px', '14px', '15px', '16px', '19px', '19px']}
+            overflow={'hidden'}
+            whiteSpace='nowrap'
+            textOverflow={'ellipsis'}
+            maxW={'17rem'}
+          >
+            {title}
+          </Text>
+        </Box>
+        <Box display='flex' gap='7px'>
+          <Text
+            fontWeight='bold'
+            fontSize={['16px', '17px', '19px', '18px', '21px', '21px']}
+          >
+            Rs.{price}
+          </Text>
+          <Text
+            fontWeight='bold'
+            fontSize={['16px', '17px', '19px', '18px', '21px', '21px']}
+            color='gray'
+            textDecoration='line-through'
+          >
+            {ogprice}
+          </Text>
+        </Box>
+      </Box>
+    </Link>
+  )
 }
 export default ProductCart
